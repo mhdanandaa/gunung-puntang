41b22845860706bdc3bf7b7547e872e3a7734228
Mhd. Ananda Ridho Alfadillah <fadil.fadil072017@gmail.com>
2025-06-04 10:29:55 +0700

    Membuat Halaman Tentang Kami

--- src/hooks/AllRouting.jsx
+++ src/hooks/AllRouting.jsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router-dom";
-import HomePage from "../pages/HomePage";
+import AboutPage from "../pages/User/AboutPage";
+import HomePage from './../pages/User/HomePage';
 
 const AllRouting = () => {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
+      <Route path="/about" element={<AboutPage />} />
     </Routes>
   );
 };
---
+++ src/pages/User/AboutPage.jsx
@@ -0,0 +1,17 @@
+import Banner from '../../components/AboutPage/Banner'
+import MainContent from '../../components/AboutPage/MainContent'
+import Footer from '../../components/Footer'
+import Navbar from '../../components/Navbar'
+
+const AboutPage = () => {
+  return (
+    <div>
+        <Navbar />
+        <Banner />
+        <MainContent />
+        <Footer />
+    </div>
+  )
+}
+
+export default AboutPage
