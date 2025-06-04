21e63057819de3aecf7ab63ba15f77e77dc262f1
Mariana <marmar.smsr@gmail.com>
2025-06-05 00:15:23 +0700

    Membuat Tampilan About Admin

--- src/hooks/AllRouting.jsx
+++ src/hooks/AllRouting.jsx
@@ -3,6 +3,10 @@ import AboutPage from "../pages/User/AboutPage";
 import HomePage from './../pages/User/HomePage';
 import ListWisataPage from "../pages/User/ListWisataPage";
 import Dashboard from './../pages/admin/Dashboard';
+import Navbar from "../pages/admin/Navbar";
+import AboutA from "../pages/admin/AboutA";
+import SidebarA from "../pages/admin/SidebarA";
+import FormAboutA from './../pages/admin/FormAboutA';
 
 const AllRouting = () => {
   return (
@@ -12,6 +16,10 @@ const AllRouting = () => {
       <Route path="/destination" element={<ListWisataPage />} />
 
       <Route path="/dashboardA" element={<Dashboard />} />
+      <Route path="/navbarA" element={<Navbar />} />
+      <Route path="/sidebarA" element={<SidebarA />} />
+      <Route path="/aboutA" element={<AboutA />} />
+      <Route path="/formAbout" element={<FormAboutA />} />
     </Routes>
   );
 };
