f2250bea20f32eee1283a9f349f3969606fdab0c
Mariana <marmar.smsr@gmail.com>
2025-06-05 00:45:04 +0700

    Memperbaharui Tampilan Navbar untuk Dashboard

--- src/pages/admin/Navbar.jsx
+++ src/pages/admin/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { MdOutlineArrowBack } from "react-icons/md";
+import { MdOutlineArrowBack, MdLogout  } from "react-icons/md";
 
 const Navbar = () => {
     const getTitle = () => {
@@ -9,16 +9,33 @@ const Navbar = () => {
 
         return 'Halaman Admin';
     }
+    const isDashboard = location.pathname === '/dashboardA'
+    
     return(
-        <nav className='bg-white fixed w-[65rem] pl-9 pr-12 py-3 flex justify-between ml-60 border-black border-b z-50'>
+        <nav 
+            className={`bg-white fixed pl-9 pr-12 py-3 flex justify-between border-black border-b z-50 ${
+                isDashboard ? "ml-0 w-full" : "ml-60 w-[65rem]"
+            }`}>
             <div className='flex items-center text-xl'>
                 <span className=' text-lg font-medium'>{getTitle()}</span>
             </div>
-            <a href="" className="flex items-center text-[#CB0404] text-lg font-medium">
-                <MdOutlineArrowBack size={25} className="mr-2"/>
-                Kembali
-            </a>
+
+            {!isDashboard && (
+                <a href="" className="flex items-center text-[#CB0404] text-lg font-medium">
+                    <MdOutlineArrowBack size={25} className="mr-2"/>
+                    Kembali
+                </a>
+            )}
+            
+            {isDashboard && (
+                <a href="" className="flex items-center text-[#FF0000] text-base font-normal">
+                    <MdLogout size={20} className="mr-2"/>
+                    Logout
+                </a>
+            )}
+
         </nav>
+        
     )
 }
 
