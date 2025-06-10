55bb1455a9afbb3a0a0567a6f325b2ed635bb7d5
Mhd. Ananda Ridho Alfadillah <fadil.fadil072017@gmail.com>
2025-06-10 12:32:12 +0700

    Memperbaiki Komponene List Wisata

--- src/components/ListWisataPage/ListWisata.jsx
+++ src/components/ListWisataPage/ListWisata.jsx
@@ -1,11 +1,12 @@
 import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 const ListWisata = () => {
   const [dataDestinasi, setDataDestinasi] = useState([]);
 
   const fetchData = async () => {
     try {
-      const response = await fetch("/public/data-puntang.json")
+      const response = await fetch("/data-puntang.json");
       if (!response.ok)
         throw new Error(`HTTP error! status: ${response.status}`);
       const data = await response.json();
@@ -23,7 +24,10 @@ const ListWisata = () => {
     return <div className="text-center py-10">Loading...</div>;
 
   return (
-    <div className="max-w-5xl mx-auto px-4 sm:px-6 md:px-14 py-14">
+    <div
+      id="listWisata"
+      className="max-w-5xl mx-auto px-4 sm:px-6 md:px-14 py-14"
+    >
       <h2 className="text-2xl font-bold mb-4">Destinasi</h2>
       <p className="text-sm text-gray-600 mb-6">
         Beberapa destinasi yang terdapat di Gunung Puntang menawarkan pengalaman
@@ -48,9 +52,11 @@ const ListWisata = () => {
                 {destinasi.deskripsi.slice(0, 100)}...
               </p>
               <div className="flex justify-end">
-                <button className="bg-orange-500 text-white text-sm px-4 py-2 rounded hover:bg-orange-600">
-                  selengkapnya
-                </button>
+                <Link to={`/detail-destination/${destinasi.id}`}>
+                  <button className="bg-orange-500 text-white text-sm px-4 py-2 rounded hover:bg-orange-600">
+                    selengkapnya
+                  </button>
+                </Link>
               </div>
             </div>
           </div>
