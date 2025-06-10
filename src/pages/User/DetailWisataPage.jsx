dfc609bca15e13de4d137c0a3a2d05686df4ed5f
Mhd. Ananda Ridho Alfadillah <fadil.fadil072017@gmail.com>
2025-06-10 12:31:23 +0700

    Halaman Detail Wisata

---
+++ src/pages/User/DetailWisataPage.jsx
@@ -0,0 +1,15 @@
+import Navbar from '../../components/Navbar'
+import DetailContent from '../../components/DetailPage/DetailContent'
+import Footer from '../../components/Footer'
+
+const DetailWisataPage = () => {
+  return (
+    <div>
+        <Navbar />
+        <DetailContent />
+        <Footer />
+    </div>
+  )
+}
+
+export default DetailWisataPage
